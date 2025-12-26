ababa7a5585400f1b0aab0c2696b48b66ed819ed
Irpear <iirsanson@gmail.com>
2025-12-26 18:20:40 +0100

    shop prijzen

--- Assets/Scripts/Shopmanager.cs
+++ Assets/Scripts/Shopmanager.cs
@@ -1,3 +1,4 @@
+using TMPro;
 using UnityEngine;
 
 public class ShopManager : MonoBehaviour
@@ -9,6 +10,29 @@ public class ShopManager : MonoBehaviour
     public int cornPrice = 3;
     public int grapePrice = 4;
     public int potatoPrice = 2;
+
+    [Header("Price Text Fields")]
+    public TextMeshProUGUI carrotPriceText;
+    public TextMeshProUGUI tomatoPriceText;
+    public TextMeshProUGUI wheatPriceText;
+    public TextMeshProUGUI cornPriceText;
+    public TextMeshProUGUI grapePriceText;
+    public TextMeshProUGUI potatoPriceText;
+
+    void Start()
+    {
+        UpdatePriceTexts();
+    }
+
+    public void UpdatePriceTexts()
+    {
+        if (carrotPriceText != null) carrotPriceText.text = carrotPrice.ToString();
+        if (tomatoPriceText != null) tomatoPriceText.text = tomatoPrice.ToString();
+        if (wheatPriceText != null) wheatPriceText.text = wheatPrice.ToString();
+        if (cornPriceText != null) cornPriceText.text = cornPrice.ToString();
+        if (grapePriceText != null) grapePriceText.text = grapePrice.ToString();
+        if (potatoPriceText != null) potatoPriceText.text = potatoPrice.ToString();
+    }
 
     public void BuySeed(string seedType, int price)
     {
